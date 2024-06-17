050da707bde696ce6f99a10e51f2e6895a10da59
Ayman Berkane <302126671@student.rocmondriaan.nl>
2024-06-17 12:46:05 +0200

    .
    .

---
+++ t-p4/public/js/cheap.js
@@ -0,0 +1,34 @@
+console.log('cheap loaded');
+
+const cardRow = document.querySelector('.card-container');
+
+fetch('/cheap-topics/')
+    .then(data => data.json())
+    .then(jsonData => showtopics(jsonData));
+
+function showtopics(topics){
+    console.log(topics);
+
+    let htmlCode = '';
+    for (let i = 0; i < topics.length; i++) {
+        const icecream = topics[i];
+        if(icecream.price < 5){
+            htmlCode += createCard(icecream);
+        }
+    }
+    cardRow.innerHTML = htmlCode;
+}
+
+function createCard(icecream){
+    const card = `
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">${icecream.icecream}</h4>
+                    <p class="card-text">${icecream.description}</p>
+                    <p class="card-text">€ ${icecream.price}</p>
+                </div>
+            </div>
+        </div>`;
+    return card;
+}
